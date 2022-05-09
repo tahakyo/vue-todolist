caeda2c1123b974c10042b645c6faa45529ac558
tahakyo <ghazouani1988@email.com>
2022-05-09 17:21:47 +0200

    create toDoList object array in js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,25 @@ const app = new Vue (
     {
         el : '#root',
         data : {
-
+            
+            toDoList : [
+                {
+                    toDo : 'Primo compito da svolgere',
+                    done : true,
+                },
+                {
+                    toDo : 'Secondo compito da svolgere',
+                    done : false,
+                },
+                {
+                    toDo : 'terzo compito da svolgere',
+                    done : true,
+                },
+                {
+                    toDo : 'terzo compito da svolgere',
+                    done : false,
+                }
+            ]
         },
         methods : {
             
